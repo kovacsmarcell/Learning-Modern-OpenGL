7d3762d66572f0ff49c56f9a29fc356c10ae67af
kovacsmarcell99 <kovacs.marcell99@gmail.com>
2014-06-11 15:15:10 +0200

    Commiting Files

---
+++ ModernOpenGL/Shader.h
@@ -0,0 +1,22 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+	Shader(const std::string& fileName);
+	~Shader();
+
+	void Bind();
+
+private:
+	static const unsigned int NUM_SHADERS = 2;
+
+	GLuint m_program;
+	GLuint m_shaders[NUM_SHADERS];
+};
+
+#endif
---
+++ ModernOpenGL/Texture.h
@@ -0,0 +1,18 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <string>
+#include <GL/glew.h>
+
+class Texture
+{
+public:
+	Texture(const std::string& fileName);
+	~Texture();
+
+	void Bind(unsigned int unit);
+private:
+	GLuint m_texture;
+};
+
+#endif

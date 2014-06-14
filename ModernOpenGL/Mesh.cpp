81a8c2118c383048611c8b45cbbd14732c005ec3
kovacsmarcell99 <kovacs.marcell99@gmail.com>
2014-06-14 10:13:32 +0200

    Light

--- ModernOpenGL/Mesh.cpp
+++ ModernOpenGL/Mesh.cpp
@@ -10,50 +10,13 @@ Mesh::Mesh(Vertex* vertices, unsigned int numVertices, unsigned int* indices, un
 	{
 		model.positions.push_back(*vertices[i].GetPos());
 		model.texCoords.push_back(*vertices[i].GetTexCoord());
+		model.normals.push_back(*vertices[i].GetNormal());
 	}
 
 	for (unsigned int i = 0; i < numIndices; i++)
 		model.indices.push_back(indices[i]);
 
 	InitMesh(model);
-
-	//m_drawCount = numIndices;
-
-	//glGenVertexArrays(1, &m_vertexArrayObject);
-	//glBindVertexArray(m_vertexArrayObject);
-
-	//std::vector<glm::vec3> positions;
-	//std::vector<glm::vec2> texCoords;
-
-	//positions.reserve(numVertices);
-	//texCoords.reserve(numVertices);
-
-	//for (unsigned int i = 0; i < numVertices; i++)
-	//{
-	//	positions.push_back(*vertices[i].GetPos());
-	//	texCoords.push_back(*vertices[i].GetTexCoord());
-	//}
-
-	////Generate Buffers
-	//glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);
-	////Position Buffer
-	//glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[POSITION_VB]);
-	//glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(positions[0]), &positions[0], GL_STATIC_DRAW);
-
-	//glEnableVertexAttribArray(0);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	////Texture Coord Buffer
-	//glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[TEXCOORD_VB]);
-	//glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(texCoords[0]), &texCoords[0], GL_STATIC_DRAW);
-
-	//glEnableVertexAttribArray(1);
-	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_vertexArrayBuffers[INDEX_VB]);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndices * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
-
-	//glBindVertexArray(0);
 }
 
 Mesh::Mesh(const std::string& fileName)
@@ -89,6 +52,12 @@ void Mesh::InitMesh(const IndexedModel& model)
 
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[NORMAL_VB]);
+	glBufferData(GL_ARRAY_BUFFER, model.normals.size() * sizeof(model.normals[0]), &model.normals[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_vertexArrayBuffers[INDEX_VB]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, model.indices.size() * sizeof(model.indices[0]), &model.indices[0], GL_STATIC_DRAW);
--- ModernOpenGL/Mesh.h
+++ ModernOpenGL/Mesh.h
@@ -8,17 +8,20 @@
 class Vertex
 {
 public:
-	Vertex(const glm::vec3& pos, const glm::vec2& texCoord)
+	Vertex(const glm::vec3& pos, const glm::vec2& texCoord, const glm::vec3& normal = glm::vec3(0,0,0))
 	{
 		this->pos = pos;
 		this->texCoord = texCoord;
+		this->normal = normal;
 	}
 
 	inline glm::vec3* GetPos() { return &pos; }
 	inline glm::vec2* GetTexCoord() { return &texCoord; }
+	inline glm::vec3* GetNormal() { return &normal; }
 private:
 	glm::vec3 pos;
 	glm::vec2 texCoord;
+	glm::vec3 normal;
 };
 
 class Mesh
@@ -37,6 +40,7 @@ private:
 	{
 		POSITION_VB,
 		TEXCOORD_VB,
+		NORMAL_VB,
 
 		INDEX_VB,
 
